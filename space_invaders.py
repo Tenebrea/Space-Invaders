7e194f38ac7d6762d0cc405b2e28cd9b4f3d20ea
Tenebrea <andrejbulatov8673@gmail.com>
2025-05-19 11:38:47 +0300

    Fleet ready

--- space_invaders.py
+++ space_invaders.py
@@ -3,6 +3,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
@@ -11,11 +12,17 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+    
     ship = Ship(ai_settings,screen)
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    alien = Alien(ai_settings, screen)
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 run_game()
